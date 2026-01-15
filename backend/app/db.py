ccdae3ff7d1de846f61ff5dddd311c65c090a45c
Mark Chweya <chweyamark@gmail.com>
2026-01-15 13:30:52 +0300

    Updates
    Environment set up
    Project set up

---
+++ backend/app/db.py
@@ -0,0 +1,12 @@
+﻿from sqlmodel import SQLModel, create_engine, Session
+import os
+
+DB_URL = os.getenv("DB_URL", "sqlite:///./spamshield.db")
+engine = create_engine(DB_URL, echo=False)
+
+def init_db() -> None:
+    SQLModel.metadata.create_all(engine)
+
+def get_session():
+    with Session(engine) as session:
+        yield session
